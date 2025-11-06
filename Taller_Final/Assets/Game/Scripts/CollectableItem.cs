82546e7ae426e3ae07a8a09bafe260f8720b0f39
catalinaortiz-hub <catalina.ortiz@uao.edu.co>
2025-11-06 16:44:51 -0500

    sonido en los items

--- Taller_Final/Assets/Game/Scripts/CollectableItem.cs
+++ Taller_Final/Assets/Game/Scripts/CollectableItem.cs
@@ -1,21 +1,74 @@
-using System.Runtime.CompilerServices;
+//using System.Runtime.CompilerServices;
+//using UnityEngine;
+//using UnityEngine.InputSystem;
+
+//public class CollectableItem : MonoBehaviour
+//{
+//    public enum ItemType { Cruz, Dinamita }  
+//    public ItemType itemType;
+//    public int itemValue = 0;
+
+
+//    private Transform player;
+
+
+//    void Start()
+//    {
+
+//    }
+
+//    private void OnTriggerEnter(Collider other)
+//    {
+//        if (other.CompareTag("Player"))
+//        {
+//            CollectItem();
+//        }
+//    }
+
+//    void Update()
+//    {
+
+
+//    }
+
+//    void CollectItem()
+//    {
+//        if (GameManager.Instance != null)
+//        {
+//            switch (itemType)
+//            {
+//                case ItemType.Cruz:
+//                    GameManager.Instance.AddScore(itemValue);
+
+//                    break;
+
+//                case ItemType.Dinamita:
+//                    GameManager.Instance.AddScore(itemValue);
+//                    GameManager.Instance.AddItem();
+//                    break;
+//            }
+
+//        }
+
+
+
+
+//        Destroy(gameObject);
+//    }
+//}
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class CollectableItem : MonoBehaviour
 {
-    public enum ItemType { Cruz, Dinamita }  
+    public enum ItemType { Cruz, Dinamita }
     public ItemType itemType;
     public int itemValue = 0;
-    
 
-    private Transform player;
-   
-
-    void Start()
-    {
-        
-    }
+    [Header("Sonidos")]
+    public AudioClip cruzSound;      // Sonido para cruz
+    public AudioClip dinamitaSound;  // Sonido para dinamita
+    public float soundVolume = 1f;   // Volumen del sonido (0-1)
 
     private void OnTriggerEnter(Collider other)
     {
@@ -23,12 +76,6 @@ public class CollectableItem : MonoBehaviour
         {
             CollectItem();
         }
-    }
-    
-    void Update()
-    {
-
-
     }
 
     void CollectItem()
@@ -39,20 +86,20 @@ public class CollectableItem : MonoBehaviour
             {
                 case ItemType.Cruz:
                     GameManager.Instance.AddScore(itemValue);
-                    
+                    if (cruzSound != null)
+                        AudioSource.PlayClipAtPoint(cruzSound, transform.position, soundVolume);
                     break;
 
                 case ItemType.Dinamita:
                     GameManager.Instance.AddScore(itemValue);
                     GameManager.Instance.AddItem();
+                    if (dinamitaSound != null)
+                        AudioSource.PlayClipAtPoint(dinamitaSound, transform.position, soundVolume);
                     break;
             }
-            
         }
 
-        
-        
-
+        // Destruye el ítem después de reproducir el sonido
         Destroy(gameObject);
     }
 }
